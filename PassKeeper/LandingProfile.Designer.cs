caf90b8a60e5a2d5a277f4ea679f57a048547a2a
Agayev Pervin Nadir <Agaye_jz58@itstep.edu.az>
2023-10-12 09:02:40 +0400

    some changes

--- PassKeeper/LandingProfile.Designer.cs
+++ PassKeeper/LandingProfile.Designer.cs
@@ -32,22 +32,35 @@
             lbl_logo = new Label();
             label1 = new Label();
             lbl_image = new Label();
+            label2 = new Label();
+            tbox_log_in = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
             // lbl_logo
             // 
             lbl_logo.Image = Properties.Resources.Screenshot_2023_10_11_165852;
-            lbl_logo.Location = new Point(57, 20);
+            lbl_logo.Location = new Point(50, 15);
             lbl_logo.Name = "lbl_logo";
-            lbl_logo.Size = new Size(212, 41);
+            lbl_logo.Size = new Size(186, 31);
             lbl_logo.TabIndex = 2;
             // 
             // label1
             // 
             label1.Font = new Font("Segoe UI Light", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(69, 624);
+            label1.Location = new Point(60, 468);
             label1.Name = "label1";
-            label1.Size = new Size(300, 20);
+            label1.Size = new Size(262, 15);
             label1.TabIndex = 8;
             label1.Text = "a product by Product Design LLC";
             // 
@@ -55,24 +68,184 @@
             // 
             lbl_image.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
             lbl_image.Image = (Image)resources.GetObject("lbl_image.Image");
-            lbl_image.Location = new Point(92, 61);
+            lbl_image.Location = new Point(102, 67);
             lbl_image.Name = "lbl_image";
-            lbl_image.Size = new Size(149, 126);
+            lbl_image.Size = new Size(74, 74);
             lbl_image.TabIndex = 9;
             lbl_image.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // label2
+            // 
+            label2.Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(32, 60, 133);
+            label2.Location = new Point(12, 184);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 20);
+            label2.TabIndex = 10;
+            label2.Text = "First name";
+            // 
+            // tbox_log_in
+            // 
+            tbox_log_in.BorderStyle = BorderStyle.None;
+            tbox_log_in.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            tbox_log_in.ForeColor = SystemColors.InactiveCaption;
+            tbox_log_in.HideSelection = false;
+            tbox_log_in.Location = new Point(13, 207);
+            tbox_log_in.Margin = new Padding(4, 3, 4, 3);
+            tbox_log_in.Multiline = true;
+            tbox_log_in.Name = "tbox_log_in";
+            tbox_log_in.ShortcutsEnabled = false;
+            tbox_log_in.Size = new Size(204, 34);
+            tbox_log_in.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.FromArgb(32, 60, 133);
+            button1.Location = new Point(224, 207);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 34);
+            button1.TabIndex = 12;
+            button1.Text = " change";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(32, 60, 133);
+            label3.Location = new Point(13, 260);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 20);
+            label3.TabIndex = 13;
+            label3.Text = "Last name";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.ForeColor = SystemColors.InactiveCaption;
+            textBox1.HideSelection = false;
+            textBox1.Location = new Point(13, 283);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ShortcutsEnabled = false;
+            textBox1.Size = new Size(204, 34);
+            textBox1.TabIndex = 14;
+            // 
+            // button2
+            // 
+            button2.ForeColor = Color.FromArgb(32, 60, 133);
+            button2.Location = new Point(224, 283);
+            button2.Name = "button2";
+            button2.Size = new Size(76, 34);
+            button2.TabIndex = 15;
+            button2.Text = " change";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(32, 60, 133);
+            label4.Location = new Point(12, 334);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 20);
+            label4.TabIndex = 16;
+            label4.Text = "Password";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.ForeColor = SystemColors.InactiveCaption;
+            textBox2.HideSelection = false;
+            textBox2.Location = new Point(13, 357);
+            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ShortcutsEnabled = false;
+            textBox2.Size = new Size(204, 34);
+            textBox2.TabIndex = 17;
+            // 
+            // button3
+            // 
+            button3.ForeColor = Color.FromArgb(32, 60, 133);
+            button3.Location = new Point(224, 357);
+            button3.Name = "button3";
+            button3.Size = new Size(76, 34);
+            button3.TabIndex = 18;
+            button3.Text = " change";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(32, 60, 133);
+            label5.Location = new Point(13, 406);
+            label5.Name = "label5";
+            label5.Size = new Size(99, 20);
+            label5.TabIndex = 19;
+            label5.Text = "Mail";
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.ForeColor = SystemColors.InactiveCaption;
+            textBox3.HideSelection = false;
+            textBox3.Location = new Point(13, 431);
+            textBox3.Margin = new Padding(4, 3, 4, 3);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ShortcutsEnabled = false;
+            textBox3.Size = new Size(204, 34);
+            textBox3.TabIndex = 20;
+            // 
+            // button4
+            // 
+            button4.ForeColor = Color.FromArgb(32, 60, 133);
+            button4.Location = new Point(224, 431);
+            button4.Name = "button4";
+            button4.Size = new Size(76, 34);
+            button4.TabIndex = 21;
+            button4.Text = " change";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(124, 144);
+            button5.Name = "button5";
+            button5.Size = new Size(27, 14);
+            button5.TabIndex = 22;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
             // LandingProfile
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(357, 653);
+            ClientSize = new Size(312, 490);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(tbox_log_in);
+            Controls.Add(label2);
             Controls.Add(lbl_image);
             Controls.Add(label1);
             Controls.Add(lbl_logo);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "LandingProfile";
             Text = "LandingProfile";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -80,5 +253,18 @@
         private Label lbl_logo;
         private Label label1;
         private Label lbl_image;
+        private Label label2;
+        private TextBox tbox_log_in;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button2;
+        private Label label4;
+        private TextBox textBox2;
+        private Button button3;
+        private Label label5;
+        private TextBox textBox3;
+        private Button button4;
+        private Button button5;
     }
 }
